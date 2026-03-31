fdc16e48fcbfda335fdf78d78e771b1528ebb831
Aguinaldo Neto <aguinaldo.netodev@gmail.com>
2026-03-30 22:39:32 -0300

    01 - primeiro commit, gerando o projeto

---
+++ fintrack-monolith/fintrack-monolith/src/main/java/com/fintrack/FintrackMonolithApplication.java
@@ -0,0 +1,13 @@
+package com.fintrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintrackMonolithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintrackMonolithApplication.class, args);
+	}
+
+}
---
+++ fintrack-monolith/fintrack-monolith/src/test/java/com/fintrack/FintrackMonolithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fintrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintrackMonolithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
